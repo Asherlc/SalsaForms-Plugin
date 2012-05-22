f5672d79726e664678bb2d6853a04010342b2607
Asher Cohen <asherlcohen@gmail.com>
2012-05-22 13:40:16 -0700

    added css file

--- salsaforms.js
+++ salsaforms.js
@@ -17,15 +17,13 @@
       //What CSS layout should the form use?
       layout : 'two_column',
       //Should this use AddThis for the TY?
-      addThis: true,
-      //What is the addThis profile ID?
-      addThisID: '4ebc8b1c2a3a1066'
+      addThis: true
     };
 
     options = $.extend(defaults,options);
 
      //Add the CSS styles to the page
-    $('head').append('<link rel="stylesheet" href="http://assets.trilogyinteractive.com/shared/css/SalsaForms.css" type="text/css" />');
+    $('head').append('<link rel="stylesheet" href="http://assets.trilogyinteractive.com/shared/css/SalsaForms-2.0.css" type="text/css" />');
 
     //Load the addThis and Validate JS files
     $.getScript('http://s7.addthis.com/js/250/addthis_widget.js#domready=1');
@@ -47,7 +45,7 @@
       //Is there a hidden list of required fields?
       if (requiredList) {
         //Split the string into an array of required field names
-        var requiredArray = requiredList.split(',')
+        var requiredArray = requiredList.split(',');
         //For each required field name, find the appropriate field and add a class of "required"
         $.each(requiredArray, function(index, value) {
           $('[name="'+value+'"]').addClass('required');
@@ -61,13 +59,13 @@
       //Load the TY page, and slide it down
       var thankYouURL = $('[name="redirect"]').val();
       //Loading the TY page
-      $('#success_message').load(thankYouURL).slideDown;
+      $('#success_message').load(thankYouURL).slideDown();
     }
 
     //Load the AddThis stuff into the page
     function loadAddThis() {
       //First give it an array of buttons we want
-      svcs = ['email', 'google_plusone', 'tweet', 'facebook_like', 'expanded'];
+      var svcs = ['email', 'google_plusone', 'tweet', 'facebook_like', 'expanded'];
       //Initialize the variable that will hold our HTML string
       var addThisButtons  = '';
       //Cycle through the hash, and make an anchor element for each
@@ -117,7 +115,8 @@
     if (options.validate) {
       setValidationClasses();
       console.log('Form validation is set to run.');
-      form.validate();
+      //Validate the form, but don't put any ugly error messages
+      form.validate({ errorPlacement: function(error, element) {} });
     }
 
     //If the ajax option is truthy, then submit the form with ajax, hide it, and show a TY div
